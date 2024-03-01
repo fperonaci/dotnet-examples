1a15c28bc13aa563f0ba34cdec8edbd5c2982a08
fperonaci <francesco.peronaci@gmail.com>
2024-03-01 10:46:27 +0100

    Add App{Consumer,Producer}

---
+++ kafka-1/AppConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using KafkaHelpers;
+
+var bootstrapServers = "localhost:9094";
+
+var topicsManager = new TopicsManager(bootstrapServers);
+var topics = topicsManager.GetTopicsNames();
+var groupIds = topicsManager.GetConsumerGroupsIds();
+
+Console.WriteLine("Hello world!");
+
+Console.WriteLine($"Existing topics: {string.Join(", ", topics)}");
+
+Console.WriteLine($"Existing group ids: {string.Join(", ", groupIds)}");
+
+Console.WriteLine("Enter the input topic and group id");
+
+var topic = Console.ReadLine() ?? throw new NullReferenceException("Cannot read topic name");
+var groupId = Console.ReadLine() ?? throw new NullReferenceException("Cannot read group id");
+var offsetReset = AutoOffsetReset.Earliest;
+
+var consumerFactory = new ConsumerFactory<string, string>(bootstrapServers, groupId, offsetReset);
+
+using (var consumer = consumerFactory.Create())
+{
+    consumer.Subscribe(topic);
+
+    Console.WriteLine("Listening for messages..");
+
+    while (true)
+    {
+        var result = consumer.Consume(CancellationToken.None);
+        Console.WriteLine(result.Message.Key);
+        Console.WriteLine(result.Message.Value);
+    }
+}
---
+++ kafka-1/AppProducer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using KafkaHelpers;
+
+var bootstrapServers = "localhost:9094";
+
+var topicsManager = new TopicsManager(bootstrapServers);
+var topics = topicsManager.GetTopicsNames();
+
+Console.WriteLine("Hello world!");
+
+Console.WriteLine($"Topics in Kafka: {string.Join(", ", topics)}");
+
+Console.WriteLine("Enter the output topic (will create if not exists)");
+
+var topic = Console.ReadLine() ?? throw new NullReferenceException("Cannot read topic name");
+var numPartitions = 1;
+
+await topicsManager.CreateTopicIfNotExists(topic, numPartitions);
+
+var producerFactory = new ProducerFactory<string, string>(bootstrapServers);
+
+using var producer = producerFactory.Create();
+
+while (true)
+{
+    Console.WriteLine("Enter key and value of the message");
+
+    var key = Console.ReadLine() ?? throw new NullReferenceException("Cannot read key");
+    var value = Console.ReadLine() ?? throw new NullReferenceException("Cannot read value");
+
+    var message = new Message<string, string>()
+    {
+        Key = key,
+        Value = value
+    };
+
+    Console.WriteLine("Sending messages..");
+
+    producer.Produce(topic, message);
+
+    Console.WriteLine("Flushing..");
+
+    producer.Flush();
+}

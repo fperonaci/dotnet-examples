31b759678660462e0058754aeb764001f19f0a53
Francesco PERONACI (PTV Group) <francesco.peronaci@ptvgroup.com>
2024-03-18 14:55:45 +0100

    Add test-2

---
+++ test-2/App/Program.cs
@@ -0,0 +1,18 @@
+﻿
+var type1 = Type.GetType(args[0]) ?? typeof(int);
+var type2 = Type.GetType(args[1]) ?? typeof(int);
+
+var x = typeof(PoliteGuy).GetMethod("SayHello");
+
+var y = x?.MakeGenericMethod([type1, type2]);
+
+y?.Invoke(null, null);
+
+static class PoliteGuy
+{
+    public static void SayHello<T1, T2>()
+    {
+        Console.WriteLine(typeof(T1));
+        Console.WriteLine(typeof(T2));
+    }
+}

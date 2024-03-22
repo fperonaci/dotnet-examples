cb42f54292b8dfb0f9a377380fb47a863467018a
fperonaci <francesco.peronaci@gmail.com>
2024-03-22 10:04:21 +0100

    Add watcher

---
+++ watcher/App1/Program.cs
@@ -0,0 +1,28 @@
+﻿// In principle it monitors the folder and whenever a file with txt
+// extension is added (also overwrite) it triggers OnChanged method
+// Problem is that sometimes the event is triggered twice
+
+var path = "/home/fp/Desktop/watcher/data";
+
+using var watcher = new FileSystemWatcher(path);
+
+watcher.NotifyFilter = NotifyFilters.LastWrite;
+watcher.Filter = "*.txt";
+watcher.IncludeSubdirectories = true;
+watcher.EnableRaisingEvents = true;
+watcher.Changed += OnChanged;
+
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
+
+static void OnChanged(object sender, FileSystemEventArgs e)
+{
+    if (e.ChangeType != WatcherChangeTypes.Changed)
+    {
+        return;
+    }
+    Console.WriteLine($"Changed: {e.FullPath}");
+
+    using var reader = new StreamReader(e.FullPath);
+    Console.Write(reader.ReadToEnd());
+}

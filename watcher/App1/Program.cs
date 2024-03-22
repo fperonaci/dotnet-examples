643daa0c6ba0bf072ce5b6a46a0bb35109bffe2c
fperonaci <francesco.peronaci@gmail.com>
2024-03-22 10:08:08 +0100

    Add helpful links to watcher/App1

--- watcher/App1/Program.cs
+++ watcher/App1/Program.cs
@@ -2,6 +2,13 @@
 // extension is added (also overwrite) it triggers OnChanged method
 // Problem is that sometimes the event is triggered twice
 
+// https://stackoverflow.com/questions/11072295/
+// https://stackoverflow.com/questions/1764809/
+// https://asp-blogs.azurewebsites.net/ashben/31773
+// https://failingfast.io/a-robust-solution-for-filesystemwatcher-firing-events-multiple-times/
+// https://github.com/benbhall/FileSystemWatcherMemoryCache
+// https://devblogs.microsoft.com/oldnewthing/20140507-00/?p=1053
+
 var path = "/home/fp/Desktop/watcher/data";
 
 using var watcher = new FileSystemWatcher(path);

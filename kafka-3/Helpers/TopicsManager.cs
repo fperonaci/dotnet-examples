968b881ef8f021dd114ac61b0ca7077ba7bf3d66
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:16 +0200

    Add kafka-3

---
+++ kafka-3/Helpers/TopicsManager.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+
+namespace Helpers;
+
+public class TopicsManager
+{
+    private readonly AdminClientBuilder _clientBuilder;
+
+    public TopicsManager(string bootstrapServers)
+    {
+        var config = new AdminClientConfig()
+        {
+            BootstrapServers = bootstrapServers
+        };
+
+        _clientBuilder = new AdminClientBuilder(config);
+    }
+
+    public IEnumerable<string> GetTopicsNames()
+    {
+        using var client = _clientBuilder.Build();
+
+        return client.GetMetadata(TimeSpan.FromSeconds(60)).Topics.Select(x => x.Topic);
+    }
+}

eef76030473b7e2e5fb1ee81b9263b92f66222fa
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:17 +0200

    Add blob-1

---
+++ blob-1/App1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Blobs;
+
+// docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite
+
+const string ConnectionString = "UseDevelopmentStorage=True";
+const string ContainerName = "test-container";
+const string BlobName = "test-blob";
+
+var container = new BlobContainerClient(ConnectionString, ContainerName);
+
+container.CreateIfNotExists();
+
+var blob = container.GetBlobClient(BlobName);
+
+using (var stream = blob.OpenWrite(true))
+{
+    using var writer = new StreamWriter(stream);
+    writer.Write("In the end it doesn't even matter");
+}
+
+using (var stream = blob.OpenRead()) {
+    using var reader = new StreamReader(stream);
+    Console.WriteLine(reader.ReadLine());
+}

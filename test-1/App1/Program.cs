b8fa36178621c53e17eb59baae424d553e8f708f
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:14 +0200

    Add test-1

---
+++ test-1/App1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var x = CountAsYouAreGood();
+
+Console.WriteLine("Here comes the sun");
+
+x.Wait();
+
+static async Task CountAsYouAreGood()
+{
+    Console.WriteLine("Hello");
+    await Task.Delay(1000);
+    Console.WriteLine("My dear");
+}
---
+++ test-1/App2/Program.cs
@@ -0,0 +1,14 @@
+﻿var x = SaySimpleWay(1);
+
+// x.Wait();
+
+var y = SaySimpleWay(2);
+
+static async Task SaySimpleWay(int power)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"In simple {i} to the {power} terms {Math.Pow(i, power)}");
+        await Task.Delay(1000);
+    }
+}

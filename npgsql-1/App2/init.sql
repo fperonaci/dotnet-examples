5a202ed37af72bbdffc24973aeef381276e5759a
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:19 +0200

    Working on npgsql-1

--- npgsql-1/App2/init.sql
+++ npgsql-1/App2/init.sql
@@ -3,3 +3,7 @@
 -- create schema private;
 -- create type mood as enum ('happy', 'sad');
 -- create table private.people (age integer, name varchar(10), cmood mood);
+
+-- create table test_table (id integer, name varchar(50));
+-- insert into test_table values (5, 'mario');
+-- insert into test_table values (3, 'piero');

bbff07680917986ffe65e699e9c45246342b714d
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:16 +0200

    Working on npgsql-1

--- npgsql-1/App1/Program.cs
+++ npgsql-1/App1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Npgsql;
 
+// sudo systemctl start docker
 // docker run -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
 // docker exec -it <container_id> /bin/bash
 
@@ -7,5 +8,6 @@ const string ConnectionString = "Host=localhost;Port=5432;Username=postgres;Pass
 
 using (var source = NpgsqlDataSource.Create(ConnectionString))
 {
+    source.CreateCommand("DROP DATABASE IF EXISTS testdb;").ExecuteNonQuery();
     source.CreateCommand("CREATE DATABASE testdb;").ExecuteNonQuery();
 }

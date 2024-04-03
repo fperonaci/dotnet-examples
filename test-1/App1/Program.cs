399a2b87be192045cf63efb26d9e2c5c5de24aaf
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:16 +0200

    Working on test-1

--- test-1/App1/Program.cs
+++ test-1/App1/Program.cs
@@ -1,13 +1,28 @@
 ﻿
-var x = CountAsYouAreGood();
+// await CountAsYouAreGood();
 
-Console.WriteLine("Here comes the sun");
+// Console.WriteLine("Here comes the sun");
 
-x.Wait();
+// static async Task CountAsYouAreGood()
+// {
+//     Console.WriteLine("Hello");
+//     await Task.Delay(1000);
+//     Console.WriteLine("My dear");
+// }
 
-static async Task CountAsYouAreGood()
+using System.Diagnostics;
+
+var sw = new Stopwatch();
+
+sw.Start();
+
+await Task.Run(() => Sleep(1));
+
+sw.Stop();
+
+Console.WriteLine(sw.Elapsed.TotalSeconds);
+
+static void Sleep(int seconds)
 {
-    Console.WriteLine("Hello");
-    await Task.Delay(1000);
-    Console.WriteLine("My dear");
+    Thread.Sleep(seconds * 1000);
 }

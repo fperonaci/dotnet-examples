215970177544fbaa0bb8875492cc0f6aa5883e60
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:18 +0200

    Working on kafka-2

--- kafka-2/App/Program.cs
+++ kafka-2/App/Program.cs
@@ -78,3 +78,56 @@ if (action == "consume")
         Console.WriteLine(timestamp);
     }
 }
+
+class TopicsManager
+{
+    private readonly AdminClientBuilder _builder;
+
+    public TopicsManager(string bootstrapServers)
+    {
+        var config = new AdminClientConfig()
+        {
+            BootstrapServers = bootstrapServers
+        };
+
+        _builder = new AdminClientBuilder(config);
+    }
+
+    public IEnumerable<string> GetConsumerGroupsIds()
+    {
+        using var client = _builder.Build();
+
+        return client.ListConsumerGroupsAsync().Result.Valid.Select(x => x.GroupId);
+    }
+
+    public IEnumerable<string> GetTopicsNames()
+    {
+        using var client = _builder.Build();
+
+        return client.GetMetadata(TimeSpan.FromSeconds(60)).Topics.Select(x => x.Topic);
+    }
+
+    public void CreateTopicIfNotExists(string name, int numPartitions)
+    {
+        if (GetTopicsNames().Contains(name))
+        {
+            return;
+        }
+
+        using var client = _builder.Build();
+
+        client.CreateTopicsAsync([new() { Name = name, NumPartitions = numPartitions }]).Wait();
+    }
+
+    public void DeleteTopicIfExists(string name)
+    {
+        if (!GetTopicsNames().Contains(name))
+        {
+            return;
+        }
+
+        using var client = _builder.Build();
+
+        client.DeleteTopicsAsync([name]).Wait();
+    }
+}

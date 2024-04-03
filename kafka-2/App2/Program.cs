6a2517cf0792a315c827969d73390e038500bdc3
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:16 +0200

    Working on kafka-2

---
+++ kafka-2/App2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text;
+using System.Text.Json;
+
+var x = new Animal() { Name = "Tobia", Age = 13 };
+
+var y = JsonSerializer.Serialize(x);
+
+Console.WriteLine(y);
+
+var z = Encoding.UTF8.GetBytes(y);
+
+z.ToList<byte>().ForEach(x => Console.WriteLine(x));
+
+class Animal
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}

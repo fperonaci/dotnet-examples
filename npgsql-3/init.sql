b48718b6fd5a2f198cabcd24fb835c1819981685
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:19 +0200

    Remove npgsql-3

--- npgsql-3/init.sql
+++
@@ -1,4 +0,0 @@
-create database test_db_01;
-create table test_table_01 (id integer, name varchar(50));
-insert into test_table_01 values (5, 'mario');
-insert into test_table_01 values (3, 'piero');

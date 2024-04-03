1adb93ac730a5ff748c4f47af41e81a750c0f3a7
fperonaci <francesco.peronaci@gmail.com>
2024-04-03 10:28:16 +0200

    Add npgsql-1

---
+++ npgsql-1/App1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Npgsql;
+
+// docker run -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+// docker exec -it <container_id> /bin/bash
+
+const string ConnectionString = "Host=localhost;Port=5432;Username=postgres;Password=postgres;";
+
+using (var source = NpgsqlDataSource.Create(ConnectionString))
+{
+    source.CreateCommand("CREATE DATABASE testdb;").ExecuteNonQuery();
+}
